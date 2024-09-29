214fcab11194ddfb2f9459cd69d337295151cd84
Torin Sparks <tsparks9038@conestogac.on.ca>
2024-09-29 06:13:57 -0400

    Parse JSON response

--- app/src/main/java/com/toadnugget/r_a_diotv/MainActivity.kt
+++ app/src/main/java/com/toadnugget/r_a_diotv/MainActivity.kt
@@ -32,6 +32,8 @@ import java.nio.charset.Charset
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
+//comment for git
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
