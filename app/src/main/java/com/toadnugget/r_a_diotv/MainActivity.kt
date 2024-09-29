b1155cff2fdc8d4ec4912e91b2235a2ad2ca8464
Torin Sparks <tsparks9038@conestogac.on.ca>
2024-09-29 06:37:43 -0400

    Fixed formatting

--- app/src/main/java/com/toadnugget/r_a_diotv/MainActivity.kt
+++ app/src/main/java/com/toadnugget/r_a_diotv/MainActivity.kt
@@ -32,8 +32,6 @@ import java.nio.charset.Charset
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-//test
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
